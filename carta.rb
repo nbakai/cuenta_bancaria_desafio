7f8c8f579953ad25e8c6eebb0e1948adc9cea0c9
Melisa Budini <melixbq@gmail.com>
2020-09-07 22:05:48 -0300

    carta y cuenta agregados

--- carta.rb
+++ carta.rb
@@ -1,6 +1,20 @@
-class Carta(numero,pinta)
+class Carta
     attr_accessor :numero, :pinta
 
-    def initialize
+    def initialize(numero,pinta)
+        @numero = numero
+        
+        @pinta = pinta
     end
+   
 end
+
+num= 0
+
+pin = ['C','D','E','T']
+num = Carta.new(num.rand(1,13))
+pin = Carta.new(pin.sample)
+
+
+puts carta.numero
+puts carta.pinta
---
+++ cuenta.rb
@@ -0,0 +1,5 @@
+clase CuentaBancaria
+    attr_accessor :nombre_de_usuario
+
+    def initialize(nombre_usuario, numero_de_cuenta)
+end
