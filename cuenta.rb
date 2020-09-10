0a780470dfa28833b5cf71a826deaf6df7638c82
Melisa Budini <melixbq@gmail.com>
2020-09-10 20:20:08 -0300

    cuenta agregada

--- cuenta.rb
+++ cuenta.rb
@@ -1,5 +1,28 @@
-clase CuentaBancaria
-    attr_accessor :nombre_de_usuario
+class CuentaBancaria
+    attr_accessor :nombre_de_usuario, :numero_de_cuenta, :vip
 
-    def initialize(nombre_usuario, numero_de_cuenta)
+    def initialize(nombre_usuario, numero_de_cuenta, vip=0)
+        numero_cuenta = numero_de_cuenta.digits
+        if numero_cuenta.count != 8
+            raise RangeError, "El número de cuenta debe tener 8 dígitos"
+        else
+            @nombre_de_usuario = nombre_de_usuario
+            @numero_de_cuenta = numero_de_cuenta
+            @vip = vip
+        end
+
+    end
+   
+    def numero_de_cuenta
+        if @vip == 1
+            puts "1-#{@numero_de_cuenta}"
+        else
+            puts "0-#{@numero_de_cuenta}"
+        end
+    end
+
 end
+
+
+numerocuenta = CuentaBancaria.new('melisa',90345678,1)
+puts numerocuenta.numero_de_cuenta
